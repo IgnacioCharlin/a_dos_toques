c0bd4f69cfe27d8d59fe9c6d11ca39196501b5b2
Ignacio Charlin <67008931+IgnacioCharlin@users.noreply.github.com>
2022-12-24 14:41:05 -0300

    Develop (#1)
    PreEntrega1 + Charlin

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ItemListContainer = ({greeting}) => {
+  return <h3> Hola <span style={{fontWeight: 'bold'}}>{greeting} </span></h3>
+}
+
+export default ItemListContainer
