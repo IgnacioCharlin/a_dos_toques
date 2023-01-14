c6d06272fedb92bda5f536759bd8d104dd77eaad
ignaciocharlin1995@gmail.com <ignaciocharlin1995@gmail.com>
2023-01-14 18:38:38 -0300

    finalizando segunda pre entrega

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,30 @@
+import { Card, Button } from "react-bootstrap";
+import ItemCount from "./ItemCount";
+import "./css/ItemDetail.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+const ItemDetail = ({ product }) => {
+  return (
+    <Card className="text-center" border="light" style={{ width: "18rem" }}>
+      <Link to="/">
+        <Button variant="danger" className="salir position-abosulute">
+          <FontAwesomeIcon className="icono" icon={faTimes} />
+        </Button>
+      </Link>
+      <Card.Img
+        className="imagen-productos"
+        variant="top"
+        src={product.pictureUrl}
+      />
+      <Card.Body className="text-center ">
+        <Card.Title>{product.title}</Card.Title>
+        <Card.Text>{product.description}</Card.Text>
+        <ItemCount />
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ItemDetail;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ItemList from "./ItemList";
+import data from "./assert/data.json";
+import { Spinner } from "react-bootstrap";
+import "./css/ItemListContainer.css";
+import { useParams } from "react-router-dom";
 
-const ItemListContainer = ({greeting}) => {
-  return <h3> Hola <span style={{fontWeight: 'bold'}}>{greeting} </span></h3>
-}
+const ItemListContainer = ({ greeting }) => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [productByCategory, setProductByCategory] = useState([]);
+  const { categoryId } = useParams();
 
-export default ItemListContainer
+  const getProductos = new Promise((res, rej) => {
+    setTimeout(() => {
+      res(data);
+    }, 2000);
+  });
+
+  useEffect(() => {
+    getProductos
+      .then((response) => {
+        setLoading(true);
+        return setProducts(response);
+      })
+      .catch((error) => console.log(error));
+  },[]);
+
+  useEffect(() => {
+    console.log(categoryId)
+    console.log(products);
+    if(categoryId !== undefined){
+      setProductByCategory(products.filter((p) => p.category == categoryId))
+    }
+  }, [categoryId]);
+
+  return (
+    <>
+      {/*}
+      <h3>
+        Hola <span style={{ fontWeight: "bold" }}>{greeting} </span>
+      {loading?<ItemList products={products} />:<Spinner color="light" />}
+  </h3>*/}
+
+      {loading?<ItemList products={categoryId ? productByCategory : products} />:<Spinner color="light" />}
+    </>
+  );
+};
+
+export default ItemListContainer;
